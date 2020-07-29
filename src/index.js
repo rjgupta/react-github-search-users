85a94a1adc0aed0074e9ce2e9d6aba25c5087f36
Rajat Gupta <imrajatgupta@gmail.com>
2020-07-29 16:13:58 -0400

    Auth0 basic setup complete

--- src/index.js
+++ src/index.js
@@ -6,11 +6,20 @@ import * as serviceWorker from './serviceWorker';
 import { GithubProvider } from './context/context';
 import { Auth0Provider } from '@auth0/auth0-react';
 
+// Domain: rjgupta21.us.auth0.com
+// Client ID: bNCotIIo6vUaIU4fHajKRBMAl8S2LmZo
+
 ReactDOM.render(
   <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+      domain='rjgupta21.us.auth0.com' 
+      clientId='bNCotIIo6vUaIU4fHajKRBMAl8S2LmZo' 
+      redirectUri={window.location.origin}
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
