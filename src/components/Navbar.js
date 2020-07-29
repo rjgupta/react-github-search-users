b5ef1d6b48a6cce8e36314f9e1481cde5669021e
Rajat Gupta <imrajatgupta@gmail.com>
2020-07-29 16:37:30 -0400

    Login, Logout add with Auth0

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,43 @@ import styled from 'styled-components';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const Navbar = () => {
+  const {isAuthenticated, loginWithRedirect, logout, user, isLoading} = useAuth0();
+  // console.log({isAuthenticated, isLoading, user});
+  const isUser = isAuthenticated && user;
+
+
   return (
     <Wrapper>
-      <h3>navbar component</h3>
+      {
+        isUser && 
+        user.picture && 
+        <img src={user.picture} alt={user.name} />
+      }
+
+      {
+        isUser && 
+        user.name && 
+        <h4>Welcome, 
+          <strong>
+            {user.name.toUpperCase()}
+          </strong> 
+        </h4>}
+
+      {isUser ? (
+        <button 
+          onClick={() => {
+            logout({returnTo:window.location.origin})
+          }}
+        >
+          logout
+        </button>
+      ) : (
+        <button 
+          onClick={loginWithRedirect}
+        >
+          login
+        </button>
+      )}
     </Wrapper>
   )
 };
